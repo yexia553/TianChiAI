55bed18e2f457da7853f19cc7520a58eef3c4796
yexia553 <1906390603@qq.com>
2020-09-30 15:57:21 +0800

    add numpy

---
+++ numpy_basic_knowledge/numpy_basic.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# 创建一个2行3列的随机数组
+# data = np.random.rand(2, 3)
+# print(data)
+# print(data.shape)
+# print(data.dtype)
+
+
+# 将现有的list转化为数组
+# data1 = [6, 7.5, 8, 0, 1]
+# data2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+# arr1 = np.array(data1)
+# arr2 = np.array(data2)
+# print(arr1, arr1.shape, arr1.dtype)
+# print(arr2, arr2.shape, arr2.dtype)
+
+
